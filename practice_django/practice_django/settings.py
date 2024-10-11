78d4b74c1cdcbf2b5db6f8cc292051ac65442477
chpdd <dgddf@g>
2024-10-11 12:02:20 +0300

    Migration from SQLite to PostgreSQL

--- practice_django/practice_django/settings.py
+++ practice_django/practice_django/settings.py
@@ -74,10 +74,18 @@ WSGI_APPLICATION = 'practice_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'practice_django_db',
+        'USER': 'postgres',
+        'PASSWORD': '12344321',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 # Password validation
